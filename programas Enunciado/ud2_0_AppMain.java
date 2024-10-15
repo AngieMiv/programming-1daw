8ac461e07ba3f3ecf19999eb4fef0d9f227ac521
AngieMiv <angieibarrola@gmail.com>
2024-10-15 15:46:48 +0200

    ej201

--- programas Enunciado/ud2_0_AppMain.java
+++ programas Enunciado/ud2_0_AppMain.java
@@ -11,10 +11,10 @@ public class ud2_0_AppMain {
 	 */
 	public void run() {
 	
-		System.out.println("Inicio de la aplicación");
+		System.out.println("Inicio de la aplicación\n");
 		/*  Insertar aquí las llamadas a los ejercicios que se vayan implementando */
 		ej201();
-		System.out.println("Fin de la aplicación");
+		System.out.println("\nFin de la aplicación");
 		
 	}
 	
@@ -23,7 +23,28 @@ public class ud2_0_AppMain {
 	public void ej201() { // Averiguar tipo de triángulo pidiendo 3 lados: a, b, c.
 		Scanner sc = new Scanner(System.in);
 		
-
+		System.out.println("Averigua qué tipo de triángulo es ingresando los lados");
+		
+		String str = "Dime el lado ";
+		System.out.println(str + "a:");
+		double a = sc.nextDouble();
+		System.out.println(str + "b:");
+		double b = sc.nextDouble();
+		System.out.println(str + "c:");
+		double c = sc.nextDouble();
+		
+		if (a == b && b == c && a == c)
+			System.out.print("Es un Triángulo Equilátero"); // 3 + 3 + 3
+		else if ((a * a + b * b) == c * c)
+			System.out.println("Es un Triángulo Rectángulo"); // 3 + 4 + 5
+		else if (a == b || b == c || a == c)
+			System.out.println("Es un Triángulo Isósceles"); // 3 + 3 + 4
+		else
+			System.out.println("Es un Triángulo Escaleno"); // 3 + 5 + 7
+		
+		System.out.println("\nYa sabes qué tipo de triángulo es ^-^");
+		
+		sc.close();
 	}
 	
 }
